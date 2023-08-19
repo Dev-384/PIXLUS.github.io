33f2c34855b5191bb5ab4ffe1fd34e543de50962
Dev-384 <106610123+Dev-384@users.noreply.github.com>
2023-08-19 17:32:00 -0400

    Uploaded all Assets
    Testing local asset fetching

--- code/play/preload/assets/fonts/index.js
+++
@@ -1,9 +0,0 @@
-import letters from "./letters.js";
-import numbers from "./numbers.js";
-import icons from "./icons.js";
-
-export default function(assetDIV=HTMLDivElement){
-	letters(assetDIV);
-	numbers(assetDIV);
-	icons(assetDIV);
-};
